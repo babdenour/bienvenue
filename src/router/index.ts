0ca403ddc5c78db03a99f1741ad7a4c578c0f901
abdenour <abdenour@hellodoe.co>
2021-05-18 23:20:36 +0200

    maj to min

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,23 +4,23 @@ import Home from '../views/home.vue';
 const routes: Array<RouteRecordRaw> = [
 	{
 		path: '/home',
-		name: 'Home',
+		name: 'home',
 		component: Home,
 	},
 	{
 		path: '/about',
-		name: 'About',
+		name: 'about',
 		component: () => import(/* webpackChunkName: "about" */ '../views/about.vue'),
 	},
 	{
 		path: '/studio',
-		name: 'Studio',
+		name: 'studio',
 		component: () => import(/* webpackChunkName: "studio" */ '../views/studio.vue'),
 	},
 	{
 		path: '/cultural',
-		name: 'Cultural',
-		component: () => import(/* webpackChunkName: "studio" */ '../views/cultural.vue'),
+		name: 'cultural',
+		component: () => import(/* webpackChunkName: "cultural" */ '../views/cultural.vue'),
 	},
 ];
 
